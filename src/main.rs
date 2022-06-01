894eb863cf67e954f3321f2bd4d20c2c6131c6ea
XmasApple <y4blokof@yandex.ru>
2022-06-02 00:58:32 +0300

    Using structs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,231 @@ use rand::seq::SliceRandom;
 use std::cmp;
 use std::io;
 
-//#[derive(Debug)]
+#[derive(PartialEq, Debug)]
+enum GameState {
+    Init,
+    Game,
+    Lose,
+    Win,
+}
+#[derive(Clone, PartialEq, Debug)]
+enum CellState {
+    Closed,
+    Open,
+    Flag,
+}
+
+impl Default for CellState {
+    fn default() -> Self {
+        CellState::Closed
+    }
+}
+
+#[derive(Clone, Default, Debug)]
+struct Cell {
+    mine: bool,
+    state: CellState,
+    neighbors: usize,
+}
+
+#[derive(Debug)]
+struct Game {
+    state: GameState,
+    field: Vec<Cell>,
+    field_size: usize,
+    mine_count: usize,
+}
+
+impl Game {
+    fn new(field_size: usize, mine_count: usize) -> Self {
+        let cell = Cell {
+            ..Default::default()
+        };
+        Self {
+            state: GameState::Init,
+            field: vec![cell; field_size * field_size],
+            field_size,
+            mine_count,
+        }
+    }
+
+    fn gen(&mut self, cursor: &Cursor) {
+        let field_size = self.field_size;
+        let mine_count = self.mine_count;
+
+        let mut indices = (0..field_size * field_size).collect::<Vec<usize>>();
+        let (xmin, xmax) = (
+            if cursor.x == 0 { 0 } else { cursor.x - 1 },
+            cmp::min(field_size, cursor.x + 2),
+        );
+        let (ymin, ymax) = (
+            if cursor.y == 0 { 0 } else { cursor.y - 1 },
+            cmp::min(field_size, cursor.y + 2),
+        );
+        //println!("{xmin} {xmax} {ymin} {ymax}");
+        for x in xmin..xmax {
+            for y in ymin..ymax {
+                //println!("{x}:{y}");
+                let index = indices
+                    .iter()
+                    .position(|p| *p == y * field_size + x)
+                    .unwrap();
+                indices.remove(index);
+            }
+        }
+
+        println!("{indices:?}");
+        if mine_count > indices.len() {
+            self.mine_count = indices.len()
+        }
+
+        let mines_indices = indices
+            .choose_multiple(&mut rand::thread_rng(), mine_count)
+            .collect::<Vec<_>>();
+
+        for i in 0..mines_indices.len() as usize {
+            let index = mines_indices[i];
+            self.field[*index].mine = true;
+        }
+        println!("{mines_indices:?}");
+
+        println!("{self:?}");
+        for x in 0..field_size {
+            for y in 0..field_size {
+                let (xmin, xmax) = (if x == 0 { 0 } else { x - 1 }, cmp::min(field_size, x + 2));
+                let (ymin, ymax) = (if y == 0 { 0 } else { y - 1 }, cmp::min(field_size, y + 2));
+
+                self.field[y * field_size + x].neighbors = (xmin..xmax)
+                    .map(|x| {
+                        (ymin..ymax)
+                            .filter(|y| self.field[y * field_size + x].mine)
+                            .count()
+                    })
+                    .sum();
+            }
+        }
+
+        //io::stdin().read_line(&mut String::new());
+    }
+
+    fn draw(&self, cursor: &Cursor) {
+        let field_size = self.field_size;
+        let mine_count = self.mine_count;
+
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char); //position the cursor at row 1, column 1:
+        for y in 0..field_size {
+            for x in 0..field_size {
+                let char = if x == cursor.x && y == cursor.y {
+                    'o'
+                } else {
+                    let cell = &self.field[y * field_size + x];
+                    match cell.state {
+                        CellState::Closed => {
+                            if cell.mine {
+                                if self.state == GameState::Lose {
+                                    print!("{} ", "*".red());
+                                    continue;
+                                }
+                                if self.state == GameState::Win {
+                                    print!("{} ", "*".red().on_white());
+                                    continue;
+                                }
+                                CLOSED
+                            } else {
+                                CLOSED
+                            }
+                        }
+                        CellState::Open => {
+                            if cell.mine {
+                                print!("{} ", "*".black().on_red());
+                                continue;
+                            } else if cell.neighbors == 0 {
+                                ' '
+                            } else {
+                                char::from_digit(cell.neighbors.try_into().unwrap(), 10)
+                                    .unwrap_or('E')
+                            }
+                        }
+                        CellState::Flag => {
+                            print!("{} ", "F".on_red());
+                            continue;
+                        }
+                    }
+                };
+                print!("{char} ");
+            }
+            println!();
+        }
+        println!(
+            "{}",
+            mine_count
+                - self
+                    .field
+                    .iter()
+                    .filter(|c| c.state == CellState::Flag)
+                    .count()
+        );
+        //println!("{:?}", &self.field);
+    }
+
+    fn open(&mut self, cursor: &Cursor, by_user: bool) {
+        let field_size = self.field_size;
+
+        if self.field[cursor.y * field_size + cursor.x].mine {
+            self.state = GameState::Lose;
+            return;
+        }
+        if self.field[cursor.y * field_size + cursor.x].state == CellState::Closed {
+            (&mut self.field[cursor.y * field_size + cursor.x]).state = CellState::Open;
+        }
+
+        let (xmin, xmax) = (
+            if cursor.x == 0 { 0 } else { cursor.x - 1 },
+            cmp::min(field_size, cursor.x + 2),
+        );
+        let (ymin, ymax) = (
+            if cursor.y == 0 { 0 } else { cursor.y - 1 },
+            cmp::min(field_size, cursor.y + 2),
+        );
+        let mut flags = 0;
+        for x in xmin..xmax {
+            for y in ymin..ymax {
+                //println!("{x}:{y}");
+                if x == cursor.x && y == cursor.y {
+                    continue;
+                }
+                if self.field[y * field_size + x].state == CellState::Flag {
+                    flags += 1;
+                }
+            }
+        }
+        if (self.field[cursor.y * field_size + cursor.x].neighbors == flags && by_user)
+            || self.field[cursor.y * field_size + cursor.x].neighbors == 0
+        {
+            for x in xmin..xmax {
+                for y in ymin..ymax {
+                    if x == cursor.x && y == cursor.y
+                        || self.field[y * field_size + x].state != CellState::Closed
+                    {
+                        continue;
+                    } else {
+                        self.open(&Cursor { x, y }, false);
+                    }
+                }
+            }
+        }
+        if self
+            .field
+            .iter()
+            .filter(|c| !c.mine && c.state == CellState::Closed)
+            .count()
+            == 0
+        {
+            self.state = GameState::Win
+        }
+    }
+}
+
 struct Cursor {
     x: usize,
     y: usize,
@@ -16,20 +240,16 @@ const CLOSED: char = '-';
 fn main() {
     let field_size = read_int("Please enter game field size.");
     println!("Game field size is {field_size}");
-    let mut mines_count = read_int("Please enter mines count.");
-    println!("Mines count is {mines_count}");
+    let mine_count = read_int("Please enter mines count.");
+    println!("Mines count is {mine_count}");
 
-    let mut field = vec![CLOSED; field_size * field_size];
-
-    println!("{field:?}");
+    let mut game = Game::new(field_size, mine_count);
 
     let mut cursor: Cursor = Cursor { x: 0, y: 0 };
 
-    let mut genered: bool = false;
-
     let stdout = Term::buffered_stdout();
     'game_loop: loop {
-        draw(&field, &field_size, &mines_count, &cursor, true);
+        game.draw(&cursor);
         if let Ok(key) = stdout.read_key() {
             println!("{key:?}");
             match key {
@@ -44,72 +264,28 @@ fn main() {
                 Key::Char('q') => break 'game_loop,
                 Key::Escape => break 'game_loop,
                 Key::Char(' ') => {
-                    if !genered {
-                        gen_field(&field_size, &mut mines_count, &mut field, &cursor);
-                        genered = true;
+                    if game.state == GameState::Init {
+                        game.gen(&cursor);
+                        game.state = GameState::Game;
                     }
-                    if !open(&field_size, &mut mines_count, &mut field, &cursor, true) {
-                        draw(&field, &field_size, &mines_count, &cursor, false);
+                    game.open(&cursor, true);
+                    if game.state == GameState::Lose || game.state == GameState::Win {
                         break 'game_loop;
                     }
                 }
                 Key::Char('f') => {
-                    let current = &mut field[cursor.y * field_size + cursor.x];
-                    *current = match *current {
-                        CLOSED => {
-                            mines_count -= 1;
-                            'f'
-                        }
-                        'f' => {
-                            mines_count += 1;
-                            CLOSED
-                        }
-                        '*' => {
-                            mines_count -= 1;
-                            'F'
-                        }
-                        'F' => {
-                            mines_count += 1;
-                            '*'
-                        }
-                        _ => 'E',
-                    }
+                    game.field[cursor.y * field_size + cursor.x].state =
+                        match game.field[cursor.y * field_size + cursor.x].state {
+                            CellState::Closed => CellState::Flag,
+                            CellState::Flag => CellState::Closed,
+                            CellState::Open => continue,
+                        };
                 }
                 _ => (),
             }
         }
     }
-}
-
-fn draw(field: &Vec<char>, field_size: &usize, mines_count: &usize, cursor: &Cursor, closed: bool) {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); //position the cursor at row 1, column 1:
-    for y in 0..*field_size {
-        for x in 0..*field_size {
-            let char = if x == cursor.x && y == cursor.y {
-                'o'
-            } else {
-                let char = field[y * field_size + x];
-                if char == '*' {
-                    if closed {
-                        CLOSED
-                    } else {
-                        print!("{} ", "*".red());
-                        continue;
-                    }
-                } else if char == 'f' || char == 'F' {
-                    print!("{} ", "F".on_red());
-                    continue;
-                } else if char == '0' {
-                    ' '
-                } else {
-                    char
-                }
-            };
-            print!("{char} ");
-        }
-        println!();
-    }
-    println!("{mines_count}");
+    game.draw(&cursor);
 }
 
 fn read_int(s: &str) -> usize {
@@ -126,105 +302,4 @@ fn read_int(s: &str) -> usize {
             return num;
         }
     }
-}
-
-fn open(
-    field_size: &usize,
-    mines_count: &mut usize,
-    field: &mut Vec<char>,
-    cursor: &Cursor,
-    by_player: bool,
-) -> bool {
-    //println!("{:?}", cursor);
-    if field[cursor.y * field_size + cursor.x] == '*' {
-        return false;
-    }
-    let (xmin, xmax) = (
-        if cursor.x == 0 { 0 } else { cursor.x - 1 },
-        cmp::min(*field_size, cursor.x + 2),
-    );
-    let (ymin, ymax) = (
-        if cursor.y == 0 { 0 } else { cursor.y - 1 },
-        cmp::min(*field_size, cursor.y + 2),
-    );
-    let mut mines = 0;
-    let mut flags = 0;
-    for x in xmin..xmax {
-        for y in ymin..ymax {
-            //println!("{x}:{y}");
-            if x == cursor.x && y == cursor.y {
-                continue;
-            }
-
-            let current = &field[y * field_size + x];
-            match current {
-                '*' => mines += 1,
-                'F' => {
-                    if by_player {
-                        flags += 1
-                    } else {
-                        mines += 1
-                    }
-                }
-                'f' => {
-                    if by_player {
-                        flags += 1
-                    } else {
-                        mines += 1
-                    }
-                }
-                _ => (),
-            }
-        }
-    }
-    field[cursor.y * field_size + cursor.x] = char::from_digit(mines + flags, 10).unwrap();
-    if mines == 0 {
-        for x in xmin..xmax {
-            for y in ymin..ymax {
-                if x == cursor.x && y == cursor.y || field[y * field_size + x] != CLOSED {
-                    continue;
-                }
-                open(field_size, mines_count, field, &Cursor { x, y }, false);
-            }
-        }
-    }
-    true
-}
-
-fn gen_field(field_size: &usize, mines_count: &mut usize, field: &mut Vec<char>, cursor: &Cursor) {
-    let mut indices = (0..field_size * field_size).collect::<Vec<usize>>();
-    let (xmin, xmax) = (
-        if cursor.x == 0 { 0 } else { cursor.x - 1 },
-        cmp::min(*field_size, cursor.x + 2),
-    );
-    let (ymin, ymax) = (
-        if cursor.y == 0 { 0 } else { cursor.y - 1 },
-        cmp::min(*field_size, cursor.y + 2),
-    );
-    //println!("{xmin} {xmax} {ymin} {ymax}");
-    for x in xmin..xmax {
-        for y in ymin..ymax {
-            //println!("{x}:{y}");
-            let index = indices
-                .iter()
-                .position(|p| *p == y * *field_size + x)
-                .unwrap();
-            indices.remove(index);
-        }
-    }
-
-    //println!("{indices:?}");
-    if *mines_count > indices.len() {
-        *mines_count = indices.len()
-    }
-
-    let mines_indices = indices
-        .choose_multiple(&mut rand::thread_rng(), *mines_count)
-        .collect::<Vec<_>>();
-
-    for i in 0..mines_indices.len() as usize {
-        let index = mines_indices[i];
-        field[*index] = '*';
-    }
-    //println!("{mines_indices:?}");
 }
